63d267e62bf6f776f6f6586cf5b463606f463134
geeky-hypertext629 <107639960+geeky-hypertext629@users.noreply.github.com>
2022-09-17 17:01:06 +0530

    Add files via upload
    This is a simple newsletter made to add people to a list who will be receiving various interesting updates through email

---
+++ app.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const https=require("https");
+const request = require("request");
+const app=express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use("/public", express.static(__dirname + '/public'));
+
+app.get("/",function(req,res)
+{
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res)
+{
+    const fname=req.body.fName;
+    const lname=req.body.lName;
+    const email1=req.body.email;
+    const data={
+        members : [
+            {
+
+        email_address : email1,
+        status : "subscribed",
+        merge_fields : { FNAME : fname,
+            LNAME : lname
+        }
+    }
+]
+    }
+    const jsondata=JSON.stringify(data);
+    //https.request("")
+    const url="https://us13.api.mailchimp.com/3.0/lists/650cf783c1";
+    const options={
+        method :"POST",
+        auth : "subham:abb17ca966392289f521cc00fa88ec5f-us13"
+    };
+    const request =https.request(url,options,function(response)
+    {
+        if(response.statusCode===200)
+        res.sendFile(__dirname + "/success.html");
+        else
+        res.sendFile(__dirname+ "/failure.html");
+        response.on("data",function(data)
+        {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsondata);
+    request.end();
+});
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000,function(req,res)
+{
+    console.log("Server running on port 3000");
+});
+
+//abb17ca966392289f521cc00fa88ec5f-us13
+//650cf783c1
